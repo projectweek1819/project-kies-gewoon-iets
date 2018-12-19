21c792359454ab7d1fa1d4ca74abf03984bf759c
RedaBoussabat <43369864+RedaBoussabat@users.noreply.github.com>
2018-12-19 14:20:39 +0100

    check move aangemaakt

--- project/p5/bejeweled/model.js
+++ project/p5/bejeweled/model.js
@@ -4,3 +4,14 @@ class Gem {
         this.special = special
     }
 }
+
+function checkMove(grid, p, q) {
+    // zien of deze move mag gebeuren
+    swap(grid, p, q);
+    let h = horizontalChainAt(grid, {x:p.x, y: q.y});
+    let v = verticalChainAt(grid, {x:p.x, y: q.y});
+
+    if(h < 3 && v < 3){
+        swap(grid, q, p);
+    }
+}
