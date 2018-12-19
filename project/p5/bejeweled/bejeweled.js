ef17e53ccb3f1d593cf07347ff0fe546ad08dc64
Johan Molenberghs <r0728245@ucll.be>
2018-12-19 13:54:54 +0100

    first visual

--- project/p5/bejeweled/bejeweled.js
+++ project/p5/bejeweled/bejeweled.js
@@ -7,10 +7,22 @@ function createGrid(rows,collumns){
     return grid
 }
 
-function fillGrid(grid){
-    for(var y = 0;y<height(grid);y++){
-        for(var x = 0;x<width(grid);x++){
-            if
+function setup(){
+    createCanvas(400,400);
+
+}
+let grid = createGrid(8,6)
+let diameter= 10
+
+function draw(){
+    background('black')
+    fill('red')
+
+    for(let i = 0;i<grid[0].length;i++){
+        for(let j = 0; j<grid.length;j++){
+            ellipse(20+(i*50),20+(j*50),30,30)
         }
     }
-}
+
+
+                    }
