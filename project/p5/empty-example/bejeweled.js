e54d9bcc9de41335737f60c95e504486c87c60c2
BertMoors <43463644+BertMoors@users.noreply.github.com>
2018-12-19 12:58:08 +0100

    creategrid + fillgrid half

---
+++ project/p5/empty-example/bejeweled.js
@@ -0,0 +1,16 @@
+//creating a grid and filling it with jewels
+function createGrid(rows,collumns){
+    var grid = Array(rows)
+    for(var i = 0;i<rows;i++){
+        grid[i] = new Array(collumns)
+    }
+    return grid
+}
+
+function fillGrid(grid){
+    for(var y = 0;y<height(grid);y++){
+        for(var x = 0;x<width(grid);x++){
+            if
+        }
+    }
+}
