62c2ffb212cedad047582d94892970085e42fbb2
Johan Molenberghs <r0728245@ucll.be>
2018-12-19 15:30:07 +0100

    adapted bejeweled

--- project/p5/bejeweled/bejeweled.js
+++ project/p5/bejeweled/bejeweled.js
@@ -16,8 +16,9 @@ function height(grid) {
 }
 
 function fillGrid(grid){
-    for(var y = 0; y != width(grid); y++){
-        for(var x = 0; x != height(grid); x++){
+    for(var y = 0; y != height(grid); y++){
+        for(var x = 0; x != width(grid); x++){
+            if(grid[y][x] == null || grid[y][x].colour === ""){
                 switch(Math.floor(Math.random()*6+1)){
                     case 1:
                         grid[y][x] = new Gem("red",false,false)
@@ -34,7 +35,7 @@ function fillGrid(grid){
                     default:
                         grid[y][x] = new Gem("purple",false,false)
                         break;
-                
+                }
             }
         }
     }
@@ -45,15 +46,15 @@ function setup(){
     createCanvas(402,402);
     noLoop()
 }
-let grid = createGrid(8,6)
+let grid = createGrid(6,8)
 fillGrid(grid)
 console.log(grid)
 let diameter= 30
 
 function draw(){
     background('black')
-    for(let i = 0;i<grid[0].length;i++){
-        for(let j = 0; j<grid.length;j++){
+    for(let i = 0;i<grid.length;i++){
+        for(let j = 0; j<grid[0].length;j++){
             console.log(grid[i][j])
             fill(grid[i][j].colour)
             ellipse(20+(i*50),20+(j*50),diameter,diameter)
